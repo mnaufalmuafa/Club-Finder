3cb923cc85e9cd17f1cb42bbf12487caf1c42902
mnaufalmuafa <naufalmuafa77@gmail.com>
2021-09-21 11:05:20 +0700

    Membuat shadow DOM

--- src/script/component/club-list.js
+++ src/script/component/club-list.js
@@ -1,23 +1,39 @@
 import "./club-item.js";
 
 class ClubList extends HTMLElement {
+    constructor() {
+        super();
+        this.shadowDom = this.attachShadow({mode : "open"});
+    }
+
     set clubs(clubs) {
         this._clubs = clubs;
         this.render();
     }
 
     render() {
-        this.innerHTML = "";
+        this.shadowDom.innerHTML = "";
         this._clubs.forEach(club => {
             const clubItemElement = document.createElement("club-item");
             clubItemElement.club = club;
-            this.appendChild(clubItemElement);
+            this.shadowDom.appendChild(clubItemElement);
         });
     }
 
     renderError(message) {
-        this.innerHTML = "";
-        this.innerHTML += `<h2 class="placeholder">${message}</h2>`;
+        // this.shadowDom.innerHTML = `
+        // <style>
+        //     .placeholder {
+        //         font-weight: lighter;
+        //         color: rgba(0,0,0,0.5);
+        //         -webkit-user-select: none;
+        //         -moz-user-select: none;
+        //         -ms-user-select: none;
+        //         user-select: none;
+        //     }
+        // </style>
+        // `;
+        this.shadowDom.innerHTML += `<h2 class="placeholder">${message}</h2>`;
     }
 }
 
